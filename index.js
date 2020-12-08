18044e0eac3952e6a98a6bae6c391c6b18ad9026
Hexagon <hexagon.study@gmail.com>
2020-12-08 10:28:36 +0700

    oke

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Oke')
 })
-
+app.get('/post', (req, res) => {
+    res.send("Post")
+})
 app.listen(3000, () => {
     console.log("Server is running")
 })
